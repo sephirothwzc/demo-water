e0ab199a5e479b1342ec1b78f0aa91992bd05f8e
xudong <tianxud@163.com>
2021-09-15 21:06:44 +0800

    css success

--- src/demo/demo-water.tsx
+++ src/demo/demo-water.tsx
@@ -1,14 +1,29 @@
-import { FC } from 'react';
-import { Card, WingBlank, WhiteSpace, List, InputItem, Button } from 'antd-mobile';
-import useForm from 'rc-form-hooks';
-import Details from './details';
-import { useImmer } from 'use-immer';
-import { dropRight } from 'lodash';
+import { FC } from "react";
+import {
+  WingBlank,
+  WhiteSpace,
+  List,
+  InputItem,
+  Button,
+  Icon,
+} from "antd-mobile";
+
+import useForm from "rc-form-hooks";
+import Details from "./details";
+import { useImmer } from "use-immer";
+import { dropRight } from "lodash";
+import "./all.css";
 
 const DemoWater: FC = () => {
   const [itemCount, setItemCount] = useImmer([{ item: 0 }]);
   const { getFieldDecorator, validateFields } = useForm<{
-    username: string;
+    phone: string;
+    company: string;
+    range: string;
+    banner: string;
+    brand: string;
+    line: string;
+    [k: string]: string;
   }>();
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -19,113 +34,111 @@ const DemoWater: FC = () => {
   return (
     <form onSubmit={handleSubmit}>
       <WingBlank size="lg">
-        <Card>
-          <Card.Header
-            title="信息登记"
-            thumb="https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg"
-            extra={<span>信息登记</span>}
-          />
-          <Card.Body>
-            <List renderHeader={() => 'Customize to focus'}>
-              {getFieldDecorator('username', {
-                rules: [{ required: true, message: 'Please input username!' }],
-              })(<InputItem>手机号</InputItem>)}
-              {getFieldDecorator('username', {
-                rules: [{ required: true, message: 'Please input username!' }],
-              })(<InputItem>所属单位</InputItem>)}
-              {getFieldDecorator('username', {
-                rules: [{ required: true, message: 'Please input username!' }],
-              })(<InputItem>管理范围</InputItem>)}
-            </List>
-          </Card.Body>
-          {/* <Card.Footer content="footer content" extra={<div>extra footer content</div>} /> */}
-        </Card>
+        <div className="nav-view">
+          <div className="nav">信息登记</div>
+        </div>
+
+        <List>
+          {getFieldDecorator("phone", {
+            rules: [{ required: true, message: "Please input username!" }],
+          })(<InputItem placeholder="请输入">手机号</InputItem>)}
+          {getFieldDecorator("company", {
+            rules: [{ required: true, message: "Please input username!" }],
+          })(<InputItem placeholder="请输入">所属单位</InputItem>)}
+          {getFieldDecorator("range", {
+            rules: [{ required: true, message: "Please input username!" }],
+          })(<InputItem placeholder="请输入">管理范围</InputItem>)}
+        </List>
+
+        {/* <Card.Footer content="footer content" extra={<div>extra footer content</div>} /> */}
+
         <WhiteSpace size="lg" />
-        <span>警示宣传措施</span>
-        <Card>
-          <Card.Body>
-            <List>
-              {getFieldDecorator('username', {
-                rules: [{ required: true, message: 'Please input username!' }],
-              })(<InputItem>条幅(条)</InputItem>)}
-              {getFieldDecorator('username', {
-                rules: [{ required: true, message: 'Please input username!' }],
-              })(<InputItem>警示牌(块)</InputItem>)}
-              {getFieldDecorator('username', {
-                rules: [{ required: true, message: 'Please input username!' }],
-              })(<InputItem>警戒线(米)</InputItem>)}
-              {getFieldDecorator('username', {
-                rules: [{ required: true, message: 'Please input username!' }],
-              })(<InputItem>摄像头(处)</InputItem>)}
-              {getFieldDecorator('username', {
-                rules: [{ required: true, message: 'Please input username!' }],
-              })(<InputItem>广播(处)</InputItem>)}
-              {getFieldDecorator('username', {
-                rules: [{ required: true, message: 'Please input username!' }],
-              })(<InputItem>围挡(米)</InputItem>)}
-              {getFieldDecorator('username', {
-                rules: [{ required: true, message: 'Please input username!' }],
-              })(<InputItem>其他</InputItem>)}
-            </List>
-          </Card.Body>
-          {/* <Card.Footer content="footer content" extra={<div>extra footer content</div>} /> */}
-        </Card>
+        <span className="title">警示宣传措施</span>
+
+        <List>
+          {getFieldDecorator("banner", {
+            rules: [{ required: true, message: "Please input username!" }],
+          })(<InputItem placeholder="请输入">条幅(条)</InputItem>)}
+          {getFieldDecorator("brand", {
+            rules: [{ required: true, message: "Please input username!" }],
+          })(<InputItem placeholder="请输入">警示牌(块)</InputItem>)}
+          {getFieldDecorator("line", {
+            rules: [{ required: true, message: "Please input username!" }],
+          })(<InputItem placeholder="请输入">警戒线(米)</InputItem>)}
+          {getFieldDecorator("camera", {
+            rules: [{ required: true, message: "Please input username!" }],
+          })(<InputItem placeholder="请输入"> 摄像头(处)</InputItem>)}
+          {getFieldDecorator("radio", {
+            rules: [{ required: true, message: "Please input username!" }],
+          })(<InputItem placeholder="请输入">广播(处)</InputItem>)}
+          {getFieldDecorator("enclosure", {
+            rules: [{ required: true, message: "Please input username!" }],
+          })(<InputItem placeholder="请输入">围挡(米)</InputItem>)}
+          {getFieldDecorator("other", {
+            rules: [{ required: true, message: "Please input username!" }],
+          })(<InputItem placeholder="请输入">其他</InputItem>)}
+        </List>
+
+        {/* <Card.Footer content="footer content" extra={<div>extra footer content</div>} /> */}
+
         <WhiteSpace size="lg" />
-        <span>当日巡查情况</span>
-        <Card>
-          <Card.Body>
-            <List>
-              {getFieldDecorator('username', {
-                rules: [{ required: true, message: 'Please input username!' }],
-              })(<InputItem>巡查人次</InputItem>)}
-              {getFieldDecorator('username', {
-                rules: [{ required: true, message: 'Please input username!' }],
-              })(<InputItem>劝阻游人数量</InputItem>)}
-            </List>
-          </Card.Body>
-          {/* <Card.Footer content="footer content" extra={<div>extra footer content</div>} /> */}
-        </Card>
+        <span className="title">当日巡查情况</span>
+
+        <List>
+          {getFieldDecorator("second", {
+            rules: [{ required: true, message: "Please input username!" }],
+          })(<InputItem placeholder="请输入">巡查人次</InputItem>)}
+          {getFieldDecorator("num", {
+            rules: [{ required: true, message: "Please input username!" }],
+          })(<InputItem placeholder="请输入">劝阻游人数量</InputItem>)}
+        </List>
+
+        {/* <Card.Footer content="footer content" extra={<div>extra footer content</div>} /> */}
+
         <WhiteSpace size="lg" />
-        <span>当日问题情况汇总</span>
-        <Card>
-          <Card.Body>
-            {itemCount.map((p) => (
-              <>
-                <Details key={p.item} getFieldDecorator={getFieldDecorator}></Details>
-                <WhiteSpace size="lg" />
-              </>
-            ))}
-            <List.Item>
-              <Button
-                type="warning"
-                onClick={() =>
-                  setItemCount((draft) => {
-                    return dropRight(draft);
-                  })
-                }
-              >
-                删除
-              </Button>
-            </List.Item>
-            <List.Item>
-              <Button
-                type="ghost"
-                onClick={() =>
-                  setItemCount((draft) => {
-                    draft.push({ item: draft.length + 1 });
-                    return draft;
-                  })
-                }
-              >
-                添加情况汇总
-              </Button>
-            </List.Item>
-          </Card.Body>
-          {/* <Card.Footer content="footer content" extra={<div>extra footer content</div>} /> */}
-        </Card>
+        <span className="title">当日问题情况汇总</span>
+
+        {itemCount.map((p) => (
+          <>
+            <Details
+              key={p.item}
+              getFieldDecorator={getFieldDecorator}
+            ></Details>
+            <WhiteSpace size="lg" />
+          </>
+        ))}
+        <List.Item>
+          <div
+            className="delBtn"
+            onClick={() =>
+              setItemCount((draft) => {
+                return dropRight(draft);
+              })
+            }
+          >
+            <Icon type="cross-circle-o" />
+            <span className="del-margin">删除</span>
+          </div>
+        </List.Item>
+
+        <div
+          className="add-btn"
+          onClick={() =>
+            setItemCount((draft) => {
+              draft.push({ item: draft.length + 1 });
+              return draft;
+            })
+          }
+        >
+          添加情况汇总
+        </div>
+
+        {/* <Card.Footer content="footer content" extra={<div>extra footer content</div>} /> */}
+
         <WhiteSpace size="lg" />
       </WingBlank>
       <Button type="primary">提交</Button>
+      <div className="bom-view"></div>
     </form>
   );
 };
--- src/demo/details.tsx
+++ src/demo/details.tsx
@@ -1,21 +1,25 @@
-import { InputItem, List } from 'antd-mobile';
-import { FC } from 'react';
+import { InputItem, List } from "antd-mobile";
+import { FC } from "react";
 
-const Details: FC<any> = ({ getFieldDecorator }: { getFieldDecorator: any }) => {
+const Details: FC<any> = ({
+  getFieldDecorator,
+}: {
+  getFieldDecorator: any;
+}) => {
   return (
     <List>
-      {getFieldDecorator('username', {
-        rules: [{ required: true, message: 'Please input username!' }],
-      })(<InputItem>问题名称</InputItem>)}
-      {getFieldDecorator('username', {
-        rules: [{ required: true, message: 'Please input username!' }],
-      })(<InputItem>问题位置</InputItem>)}
-      {getFieldDecorator('username', {
-        rules: [{ required: true, message: 'Please input username!' }],
-      })(<InputItem>情况概述</InputItem>)}
-      {getFieldDecorator('username', {
-        rules: [{ required: true, message: 'Please input username!' }],
-      })(<InputItem>是否解决</InputItem>)}
+      {getFieldDecorator("name", {
+        rules: [{ required: true, message: "Please input username!" }],
+      })(<InputItem placeholder="请输入">问题名称</InputItem>)}
+      {getFieldDecorator("pontions", {
+        rules: [{ required: true, message: "Please input username!" }],
+      })(<InputItem placeholder="请输入">问题位置</InputItem>)}
+      {getFieldDecorator("describe", {
+        rules: [{ required: true, message: "Please input username!" }],
+      })(<InputItem placeholder="请输入">情况概述</InputItem>)}
+      {getFieldDecorator("solve", {
+        rules: [{ required: true, message: "Please input username!" }],
+      })(<InputItem placeholder="请输入">是否解决</InputItem>)}
     </List>
   );
 };
